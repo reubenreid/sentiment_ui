b235de1f8621b71ebf1475e0ea7997a1c761f6ea
Reuben Reid <mz020778@live.reading.ac.uk>
2020-04-03 16:56:17 +0100

    half working map

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Homepage from './homepage/Homepage.js'
 import Header from './Header.js'
 import Map from './map/Map.js'
 import {BrowserRouter as Router, Route} from "react-router-dom";
-
 import './App.scss';
 
+
 const HomePage = () => <Homepage/>;
-const MapPage = () => <Map/>;
+const MapPage = () => <div className="Map"><Map/></div>;
 
 function App() {
     return (
@@ -15,9 +15,7 @@ function App() {
             <div className="App">
                 <Header className="Header"/>
                 <Route exact path="/" component={HomePage}/>
-                <div className="Map">
-                    <Route path="/map" component={MapPage}/>
-                </div>
+                <Route path="/map" component={MapPage}/>
             </div>
         </Router>
     );
--- src/map/Map.js
+++ src/map/Map.js
@@ -2,18 +2,20 @@ import React from 'react';
 import DeckGL from '@deck.gl/react';
 import {HexagonLayer} from '@deck.gl/aggregation-layers';
 import {StaticMap} from 'react-map-gl';
+import {csv} from 'd3';
+import csvData from '../data/uk_online_data.csv';
 
 
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoicmV1YmVucmVpZCIsImEiOiJjazdidWRldWwwN2g3M2hwYTV4czhwMDB1In0.iPL0MGIDZ864MPRG-wDarg';
 
 const initialViewState = {
-    longitude: -122.41669,
-    latitude: 37.7853,
+    longitude: -0.15012839,
+    latitude: 51.50518452,
     zoom: 10,
-    minZoom: 5,
+    minZoom: 1,
     maxZoom: 15,
     pitch: 40.5,
-    bearing: 0,
+    bearing: -27.396674584323023,
     width: 300,
     height: 300,
 };
@@ -28,40 +30,67 @@ const viewPort = {
 
 const data =
     [
-        {COORDINATES: [-122.42177834, 37.78346622]},
+        {COORDINATES: [-0.15012839, 51.50518452]},
+        {COORDINATES: [-0.15012839, 51.50518452]},
     ]
 
 
+
 class Map extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {data: ''}
+    }
+
+    componentDidMount() {
+        this.parseFile()
+    }
+
+
+    parseFile = () => {
+        csv(csvData, (err, data) => {
+            if (err) {
+                console.log(err)
+            }
+            this.setState({data: data.map( d => [
+                {COORDINATES: [d.Longitude, d.Latitude]}
+                ])
+            })
+        });
+        console.log(this.state.data)
+    }
 
     render() {
 
         const layer = new HexagonLayer({
             id: 'hexagon-layer',
-            data,
-            pickable: true,
+            data: this.state.data !== '' ? this.state.data: data,
+            //data: data,
+            // elevationRange: [0, 1000],
+            // elevationScale: 250,
             extruded: true,
-            radius: 200,
-            elevationScale: 4,
-            getPosition: d => d.COORDINATES,
-            onHover: ({object, x, y}) => {
-                const tooltip = `${object.centroid.join(', ')}\nCount: ${object.points.length}`;
-                /* Update tooltip
-                   http://deck.gl/#/documentation/developer-guide/adding-interactivity?section=example-display-a-tooltip-for-hovered-object
-                */
-            }
+            opacity: 1,
+            radius: 1000,
+            upperPercentile: 100,
+            coverage: 1,
+            getPosition: d => d.COORDINATES
         });
+
+        console.log(data)
+        console.log(this.state.data)
 
         return (
             <DeckGL
                 initialViewState={initialViewState}
                 controller={true}
                 layers={layer}
-                width={1345}
-                height={720}
+                width={1347}
+                height={640}
             >
+                {console.log(layer)}
                 <StaticMap  {...viewPort} mapStyle={'mapbox://styles/mapbox/dark-v9'}
-                           mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}/>
+                            mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}/>
             </DeckGL>
         );
     }
